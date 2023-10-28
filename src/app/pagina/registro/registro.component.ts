439875094a8a1b79ea7ca344eabcfa8d24d007b4
David <dlromerom@uqvirtual.edu.co>
2023-10-28 02:03:39 -0500

    modificaciones al formulario de registro

--- src/app/pagina/registro/registro.component.ts
+++ src/app/pagina/registro/registro.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { RegistroPacienteDTO } from 'src/app/modelo/registro-paciente-dto';
 
 @Component({
   selector: 'app-registro',
@@ -6,5 +7,53 @@ import { Component } from '@angular/core';
   styleUrls: ['./registro.component.css']
 })
 export class RegistroComponent {
+
+  registroPacienteDTO: RegistroPacienteDTO;
+  eps: string[];
+  ciudades: string[];
+  archivos!: FileList;
+
+  private cargarEps() {
+    this.eps.push("Sanitas");
+    this.eps.push("Coomeva");
+    this.eps.push("Nueva eps");
+  }
+
+
+  private cargarCiudades() {
+    this.ciudades.push("Armenia");
+    this.ciudades.push("Calarcá");
+    this.ciudades.push("Pereira");
+    this.ciudades.push("Manizales");
+    this.ciudades.push("Medellín");
+  }
+
+  constructor() {
+    this.registroPacienteDTO = new RegistroPacienteDTO();
+    this.ciudades = [];
+    this.cargarCiudades();
+    this.eps = [];
+    this.cargarEps();
+  }
+
+  public onFileChange(event: any) {
+    if (event.target.files.length > 0) {
+      const files = event.target.files;
+      this.archivos = event.target.files;
+    }
+  }
+
+  public registrar() {
+    if (this.archivos != null && this.archivos.length > 0) {
+      console.log(this.registroPacienteDTO);
+    } else {
+      console.log("Debe cargar una foto");
+    }
+  }
+
+
+  public sonIguales(): boolean {
+    return this.registroPacienteDTO.password == this.registroPacienteDTO.confirmaPassword;
+  }
 
 }
